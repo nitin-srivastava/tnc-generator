43a1c961706a0bd4c2caee93004a3e7d644d0281
Nitin Srivastava <nitin@curvetomorrow.com>
2021-07-07 11:08:24 +0530

    execute will take dataset and template to generate T&C document

--- lib/document_generator.rb
+++ lib/document_generator.rb
@@ -3,27 +3,52 @@
 # files from the list and generate the T&C document
 require 'byebug'
 require 'yaml'
+require_relative 'document_parser'
 class DocumentGenerator
 
   def execute
+    select_dataset
+    generate_document
+  end
+
+  private
+
+  def select_dataset
     available_files = Dir['data/*.yml']
-    prompt_message(available_files)
+    prompt_message('dataset', available_files)
     loop do
       dataset_file = gets.chomp
       if available_files.include?(dataset_file)
         @dataset = YAML.load_file(dataset_file)['dataset']
         break
       else
-        prompt_message(available_files, invalid_input: true)
+        options = { input_file: dataset_file, invalid_input: true }
+        prompt_message('dataset', available_files, options)
       end
     end
   end
 
-  private
+  def generate_document
+    available_files = Dir['data/*.txt']
+    prompt_message('template', available_files)
+    loop do
+      template_file = gets.chomp
+      if available_files.include?(template_file)
+        document_parser = DocumentParser.new(@dataset['clauses'], @dataset['sections'])
+        content = document_parser.parse(template_file)
+        document_parser.generate(content)
+        printf "\nA T&C Document has been generated in tmp folder.\n"
+        break
+      else
+        options = { input_file: template_file, invalid_input: true }
+        prompt_message('template', available_files, options)
+      end
+    end
+  end
 
-  def prompt_message(files, invalid_input = false)
-    puts "Sorry wrong input." if invalid_input
-    puts "Please enter a dataset file from the below list:"
+  def prompt_message(file_type, files, options = {})
+    puts "Sorry wrong input #{options[:input_file]}." if options[:invalid_input]
+    puts "Please enter a #{file_type} file from the below list:"
     files.each_with_index { |f, i| printf "#{i + 1}. #{f}\n"}
     printf "\n"
   end
--- spec/document_generator_spec.rb
+++ spec/document_generator_spec.rb
@@ -4,23 +4,65 @@ require_relative '../lib/document_generator'
 RSpec.describe DocumentGenerator do
   subject { DocumentGenerator.new }
 
-  let(:dataset_output_msg) do
-    "Please enter a dataset file from the below list:\n1. data/dataset.yml\n\n"
-  end
-
   describe '#execute' do
-    let(:input_file) { "test_file.yml\n" }
-    let(:wrong_input_msg) do
-      "#{dataset_output_msg}Sorry wrong input.\n#{dataset_output_msg}"
+    let(:dataset_output_msg) do
+      "Please enter a dataset file from the below list:\n1. data/dataset.yml\n\n"
+    end
+    let(:output_msg) do
+      "Please enter a template file from the below list:\n1. data/template_one.txt\n2. data/template_two.txt\n\n"
+    end
+    let(:file_name) { 'spec/files/tnc_document.txt' }
+    after do
+      File.delete(file_name) if File.exist?(file_name)
     end
 
-    before do
-      allow(subject).to receive(:gets).and_return(input_file)
-      allow(subject).to receive(:loop).and_yield
+    context 'when wrong dataset file has entered then' do
+      let(:input_file) { "test_file.yml\n" }
+      let(:wrong_input_msg) do
+        "#{dataset_output_msg}Sorry wrong input #{input_file.chomp}.\n#{dataset_output_msg}"
+      end
+
+      before do
+        allow(subject).to receive(:gets).and_return(input_file)
+        allow(subject).to receive(:generate_document).and_return(true)
+        allow(subject).to receive(:loop).and_yield
+      end
+
+      it 'prompt wrong input message' do
+        expect { subject.execute }.to output(wrong_input_msg).to_stdout
+      end
     end
 
-    it 'prompt wrong input message' do
-      expect { subject.execute }.to output(wrong_input_msg).to_stdout
+    context 'when wrong file entered then' do
+      let(:input_file) { "test_file.txt\n" }
+      let(:wrong_input_msg) do
+        "#{dataset_output_msg}#{output_msg}Sorry wrong input test_file.txt.\n#{output_msg}"
+      end
+
+      before do
+        allow(subject).to receive(:gets).and_return("data/dataset.yml", input_file)
+        allow(subject).to receive(:loop).and_yield
+      end
+
+      it 'prompt wrong input message' do
+        expect { subject.execute }.to output(wrong_input_msg).to_stdout
+      end
+    end
+
+    context 'when a valid file entered then' do
+      let(:input_file) { "data/template_one.txt\n" }
+      let(:print_msg) do
+        "#{dataset_output_msg}#{output_msg}\nA T&C Document has been generated in tmp folder.\n"
+      end
+
+      before do
+        allow(subject).to receive(:gets).and_return("data/dataset.yml", input_file)
+        allow(subject).to receive(:loop).and_yield
+      end
+
+      it 'prints T&C Document has been generated in tmp folder message' do
+        expect { subject.execute }.to output(print_msg).to_stdout
+      end
     end
   end
 end
